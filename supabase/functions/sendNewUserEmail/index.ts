05e6793bc89391852f1eefcb4f6deed378aec6f5
FatumaA <67555014+FatumaA@users.noreply.github.com>
2023-04-12 13:05:19 +0300

    fix: cant read undefined - supabase deno dep issue

--- supabase/functions/sendNewUserEmail/index.ts
+++ supabase/functions/sendNewUserEmail/index.ts
@@ -2,50 +2,97 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
 import { serve } from "https://deno.land/std@0.182.0/http/server.ts";
-import { SmtpClient } from "denomailer";
-import { supabaseClient } from "https://deno.land/x/supabase_deno/mod.ts";
+import { SmtpClient } from "https://deno.land/x/smtp@v0.7.0/mod.ts";
 
-console.log("Hello from Functions!");
-const sbclient = supabaseClient(
-	"https://xlvhpbcxavkjwhvvumjr.supabase.co",
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzOTIyNDEzNSwiZXhwIjoxOTU0ODAwMTM1fQ.HZ4d4SdD40txeDY71d010S914KgrKYJNAzkrRU0C07w"
+console.log("Send New User Email Function Init!");
+
+const supaClient = createClient(
+	Deno.env.get("SUPABASE_URL")!,
+	Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!
 );
+
+console.log(`Supabase client init - ${supaClient}`);
 const smtp = new SmtpClient();
 
-serve(async (_req) => {
-	await sbclient.auth.signUp();
-	await smtp.connect({
-		hostname: Deno.env.get("SMTP_HOSTNAME")!,
-		port: Number(Deno.env.get("SMTP_PORT")!),
-		username: Deno.env.get("SMTP_USERNAME")!,
-		password: Deno.env.get("SMTP_PASSWORD")!,
-	});
+const corsHeaders = {
+	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Headers":
+		"authorization, x-client-info, apikey, content-type",
+	"Content-Type": "application/json",
+};
 
-	try {
-		await smtp.send({
-			from: Deno.env.get("SMTP_FROM")!,
-			to: "testr@test.de",
-			subject: `Hello from Supabase Edge Functions`,
-			content: `Hello Functions \o/`,
-		});
-	} catch (error) {
-		return new Response(error.message, { status: 500 });
+serve(async (req) => {
+	if (req.method === "OPTIONS") {
+		return new Response("ok", { headers: corsHeaders });
 	}
 
-	await smtp.close();
+	// create a new user
+	const { email, password, name, role } = await req.json();
 
-	return new Response(
-		JSON.stringify({
-			done: true,
-		}),
-		{
-			headers: { "Content-Type": "application/json" },
+	console.log(`About to sign up new user - req }`);
+
+	const { data, error } = await supaClient.auth.admin.createUser({
+		email,
+		password,
+		user_metadata: { name, user_role: role },
+	});
+
+	console.log(`Response from sign up - ${data?.toString()}`);
+
+	if (error) {
+		// return res.status(500).send(error.message);
+		return new Response(JSON.stringify(error), {
+			headers: corsHeaders,
+			status: 500,
+		});
+	}
+
+	if (!error) {
+		// send new user email
+		console.log(`Sending email init`);
+
+		await smtp.connectTLS({
+			hostname: "smtp.gmail.com",
+			port: 465,
+			username: Deno.env.get("SEND_EMAIL")!,
+			password: Deno.env.get("EMAIL_PWD")!,
+		});
+
+		try {
+			console.log(`Trying to send email`);
+
+			await smtp.send({
+				from: Deno.env.get("SEND_EMAIL")!,
+				to: email,
+				subject: "Acme Corp - Login Details",
+				content: `Hello ${name},\r, \r, Your login details are:\r, \r, Email: ${email}\r, Password: ${password}\r, \r, Please change your password upon logging in at http://localhost:3000/ \r, Kind Regards,\r, Acme Corp`,
+			});
+
+			console.log(`Email sent successfully`);
+		} catch (error) {
+			console.log(`Email send error - ${error.message}`);
+
+			return new Response(error.message, {
+				headers: corsHeaders,
+				status: 500,
+			});
 		}
-	);
+
+		await smtp.close();
+
+		return new Response(
+			JSON.stringify({
+				done: true,
+			}),
+			{
+				headers: corsHeaders,
+			}
+		);
+	}
+	return new Response(JSON.stringify({}), {
+		headers: corsHeaders,
+		status: 400,
+	});
 });
-// To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
