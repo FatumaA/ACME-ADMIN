cbd2caf8f32729aed290785fcbd41998975d6fce
FatumaA <67555014+FatumaA@users.noreply.github.com>
2023-04-13 22:42:05 +0300

    fix: make email work without smtp - (deno issues)

--- supabase/functions/sendNewUserEmail/index.ts
+++ supabase/functions/sendNewUserEmail/index.ts
@@ -4,7 +4,7 @@
 
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 import { serve } from "https://deno.land/std@0.182.0/http/server.ts";
-import { SmtpClient } from "https://deno.land/x/smtp@v0.7.0/mod.ts";
+// import { SmtpClient } from "https://deno.land/x/smtp@v0.7.0/mod.ts";
 
 console.log("Send New User Email Function Init!");
 
@@ -14,7 +14,7 @@ const supaClient = createClient(
 );
 
 console.log(`Supabase client init - ${supaClient}`);
-const smtp = new SmtpClient();
+// const smtp = new SmtpClient();
 
 const corsHeaders = {
 	"Access-Control-Allow-Origin": "*",
@@ -39,7 +39,7 @@ serve(async (req) => {
 		user_metadata: { name, user_role: role },
 	});
 
-	console.log(`Response from sign up - ${data?.toString()}`);
+	console.log(`Response from sign up - ${JSON.stringify(data)}`);
 
 	if (error) {
 		// return res.status(500).send(error.message);
@@ -52,38 +52,67 @@ serve(async (req) => {
 	if (!error) {
 		// send new user email
 		console.log(`Sending email init`);
-
-		await smtp.connectTLS({
-			hostname: "smtp.gmail.com",
-			port: 465,
-			username: Deno.env.get("SEND_EMAIL")!,
-			password: Deno.env.get("EMAIL_PWD")!,
+		const resp = await fetch("https://api.postmarkapp.com/email", {
+			method: "POST",
+			headers: {
+				"X-Postmark-Server-Token": `${Deno.env.get("POSTMARK_API_TOKEN")}`,
+				"content-type": "application/json",
+			},
+			body: JSON.stringify({
+				FROM: Deno.env.get("SEND_EMAIL")!,
+				TO: email,
+				SUBJECT: "Acme Corp - Login Details",
+				TEXTBODY: `Hello ${name},\r, \r, Your login details are:\r, \r, Email: ${email}\r, Password: ${password}\r, \r, Please change your password upon logging in at http://localhost:3000/ \r, Kind Regards,\r, Acme Corp`,
+			}),
 		});
 
-		try {
-			console.log(`Trying to send email`);
+		const { ErrorCode, Message } = await resp.json();
+		console.log(`Response from Postmark api - ${resp}`);
 
-			await smtp.send({
-				from: Deno.env.get("SEND_EMAIL")!,
-				to: email,
-				subject: "Acme Corp - Login Details",
-				content: `Hello ${name},\r, \r, Your login details are:\r, \r, Email: ${email}\r, Password: ${password}\r, \r, Please change your password upon logging in at http://localhost:3000/ \r, Kind Regards,\r, Acme Corp`,
-			});
-
-			console.log(`Email sent successfully`);
-		} catch (error) {
-			console.log(`Email send error - ${error.message}`);
-
-			return new Response(error.message, {
+		if (ErrorCode !== 0) {
+			console.error(
+				`Error sending email - Msg: ${Message} - Error code: ${ErrorCode}`
+			);
+			return new Response(JSON.stringify(Message), {
 				headers: corsHeaders,
 				status: 500,
 			});
 		}
 
-		await smtp.close();
+		// Dropping this implementation coz of this error:
+		// DenoStdInternalError: bufio: caught error from `readSlice()` without `partial` property
+		// await smtp.connectTLS({
+		// 	hostname: "smtp.postmarkapp.com",
+		// 	port: 2525,
+		// 	username: Deno.env.get("POSTMARK_API_TOKEN")!,
+		// 	password: Deno.env.get("POSTMARK_API_TOKEN")!,
+		// });
+
+		// try {
+		// 	console.log(`Trying to send email`);
+
+		// 	await smtp.send({
+		// 		from: Deno.env.get("SEND_EMAIL")!,
+		// 		to: email,
+		// 		subject: "Acme Corp - Login Details",
+		// 		content: `Hello ${name},\r, \r, Your login details are:\r, \r, Email: ${email}\r, Password: ${password}\r, \r, Please change your password upon logging in at http://localhost:3000/ \r, Kind Regards,\r, Acme Corp`,
+		// 	});
+
+		// 	console.log(`Email sent successfully`);
+		// } catch (error) {
+		// 	console.log(`Email send error - ${error.message}`);
+
+		// 	return new Response(error.message, {
+		// 		headers: corsHeaders,
+		// 		status: 500,
+		// 	});
+		// }
+
+		// await smtp.close();
 
 		return new Response(
 			JSON.stringify({
+				message: `${Message}`,
 				done: true,
 			}),
 			{
